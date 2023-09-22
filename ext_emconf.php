d60afbe159db5ef06929591bf739d47829b1b394
Rafal Jania <rj@studiomitte.com>
2023-09-22 10:49:36 +0200

    Add new constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,4 +9,13 @@ $EM_CONF[$_EXTKEY] = [
     'state' => 'beta',
     'clearCacheOnLoad' => true,
     'version' => '1.0.0',
+    'constraints' =>
+        [
+            'depends' => [
+                'typo3' => '12.4.0-12.4.99',
+                'cart' => '9.0.0-9.99.99'
+            ],
+            'conflicts' => [],
+            'suggests' => [],
+        ]
 ];
